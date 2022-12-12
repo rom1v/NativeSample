b3b830a43e8fd66a4b911bef50db0f0309b36530
Romain Vimont <rom@rom1v.com>
2022-12-12 15:45:12 +0100

    Workaround JNICALL

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,9 @@
 #include <jni.h>
 #include <string>
 
+// avoids "Incorrect return type JNICALL, which should have been jstring." with NDK 25 :/
+#define JNICALL
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_rom1v_nativesample_MainActivity_stringFromJNI(
         JNIEnv* env,
