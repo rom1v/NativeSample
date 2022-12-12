822036905256e71fed6f16f2bf891dead4c68a52
Romain Vimont <rom@rom1v.com>
2022-12-12 14:51:43 +0100

    Add FFmpeg dependency PoC

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,8 @@
 #include <jni.h>
 #include <string>
 
+#include <libavcodec/packet.h>
+
 // avoids "Incorrect return type JNICALL, which should have been jstring." with NDK 25 :/
 #define JNICALL
 
@@ -8,6 +10,7 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_rom1v_nativesample_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+    AVPacket packet;
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
