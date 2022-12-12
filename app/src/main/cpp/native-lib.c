8182ec524950f9cc6f4d52e50bb0b668ca059ad8
Romain Vimont <rom@rom1v.com>
2022-12-12 17:06:10 +0100

    Convert to C

--- app/src/main/cpp/native-lib.c
+++ app/src/main/cpp/native-lib.c
@@ -1,23 +1,18 @@
 #include <jni.h>
-#include <string>
-#include <cstdio>
-#include <cassert>
+#include <assert.h>
 
-extern "C" {
 #include <libavcodec/avcodec.h>
-}
 
 // avoids "Incorrect return type JNICALL, which should have been jstring." with NDK 25 :/
 #define JNICALL
 
-extern "C" JNIEXPORT jstring JNICALL
+JNIEXPORT jstring JNICALL
 Java_com_rom1v_nativesample_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     unsigned v = avcodec_version();
-    std::string hello = "Hello from C++";
     char *str;
     int r = asprintf(&str, "libavcodec: %u.%u.%u", AV_VERSION_MAJOR(v), AV_VERSION_MINOR(v), AV_VERSION_MICRO(v));
     assert(r != -1);
-    return env->NewStringUTF(str);
+    return (*env)->NewStringUTF(env, str);
 }
