282125e9f72a198283950f8235c5aa1914e3f1e8
Romain Vimont <rom@rom1v.com>
2022-12-12 15:52:19 +0100

    Call libavcodec function
    But it does not link.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,7 @@
 #include <jni.h>
 #include <string>
 
-#include <libavcodec/packet.h>
+#include <libavcodec/avcodec.h>
 
 // avoids "Incorrect return type JNICALL, which should have been jstring." with NDK 25 :/
 #define JNICALL
@@ -10,7 +10,8 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_rom1v_nativesample_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    AVPacket packet;
+    unsigned v = avcodec_version();
+    printf("libavcodec: %u.%u.%u", AV_VERSION_MAJOR(v), AV_VERSION_MINOR(v), AV_VERSION_MICRO(v));
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
